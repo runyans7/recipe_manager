de5b31cc232f8fd2bea7ad5c2106eea48bf20427
Scotty Runyan <runyans7@gmail.com>
2016-04-18 10:57:47 -0400

    Skip ingredient add if ingredient blank

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,12 @@
+require 'pry'
 class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
 
   def ingredients_attributes=(ingredients_attributes)
-    ingredients_attributes.each do |ingredient_attributes|
-      self.ingredients.build(ingredient_attributes)
+    ingredients_attributes.each do |i, ingredient_attributes|
+      binding.pry
+      self.ingredients.build(ingredient_attributes) unless ingredient_attributes["name"].blank?
     end
   end
 end
