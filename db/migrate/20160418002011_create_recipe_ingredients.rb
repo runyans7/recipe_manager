f4245b984341c011db4ea3c86bbf1a979a3088cf
Scotty Runyan <runyans7@gmail.com>
2016-04-17 20:41:43 -0400

    Models and associations setup

---
+++ db/migrate/20160418002011_create_recipe_ingredients.rb
@@ -0,0 +1,9 @@
+class CreateRecipeIngredients < ActiveRecord::Migration
+  def change
+    create_table :recipe_ingredients do |t|
+      t.references :recipe, index: true, foreign_key: true
+      t.references :ingredient, index: true, foreign_key: true
+      t.string :amount
+    end
+  end
+end
