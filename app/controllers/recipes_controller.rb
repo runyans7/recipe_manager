d2140d27de293b466649c85aff2df0b702afd40c
Scotty Runyan <runyans7@gmail.com>
2016-04-18 10:49:11 -0400

    Ingredients created with recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,33 @@
+require 'pry'
+
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+    6.times { @recipe.ingredients.new }
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, ingredients_attributes: [:name])
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,10 @@
 class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+
+  def ingredients_attributes=(ingredients_attributes)
+    ingredients_attributes.each do |ingredient_attributes|
+      self.ingredients.build(ingredient_attributes)
+    end
+  end
 end
